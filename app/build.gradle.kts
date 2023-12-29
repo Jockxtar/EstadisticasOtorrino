a88404898df2c629eece43d594e97a54b18d3b8c
Jockxtar <carlos.aran.tapia@gmail.com>
2023-12-29 17:36:26 +0100

    Tarea 2
    - Aumentado compileSdk de 33 a 34 para que no de problemas al compilar.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.jockxtar.estadisticasotorrino"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jockxtar.estadisticasotorrino"
