a91c728d0364b055b93a948477a4fb9b41399e51
Jockxtar <carlos.aran.tapia@gmail.com>
2024-01-09 18:46:39 +0100

    Tarea 3
    - Tuve que crear de nuevo el proyecto porque no me detectaba Compose.
    - Añadí:
       - Un Scaffold que contiene una topBar y una bottomBar (ambas por defecto).
       - Un texto por defecto.
       - Una lista de elementos que muestra la información de varios pacientes.
       - Una imagen de los primero que me salió buscando "Pepe" en Google.

--- app/src/main/java/com/jockxtar/estadisticasotorrino/MainActivity.kt
+++ app/src/main/java/com/jockxtar/estadisticasotorrino/MainActivity.kt
@@ -1,35 +1,166 @@
+@file:OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3Api::class
+)
+
 package com.jockxtar.estadisticasotorrino
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.jockxtar.estadisticasotorrino.databinding.ActivityMainBinding
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jockxtar.estadisticasotorrino.ui.theme.EstadisticasOtorrinoTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContent {
+            EstadisticasOtorrinoTheme {
+                MainActivityContent()
+            }
+        }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainActivityContent() {
+    Box {
+        val modifierPadding = Modifier.padding(8.dp)
+
+
+        Column {
+
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        /*colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),*/
+                        title = {
+                            Text("Top app bar")
+                        }
+                    )
+                },
+                bottomBar = {
+                    BottomAppBar(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        contentColor = MaterialTheme.colorScheme.primary,
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            text = "Bottom app bar",
+                        )
+                    }
+                }
+            )
+            { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    Text(text = "Hola Mundo")
+
+                    LazyColumn {
+                        item {
+                            Text("Lista de pacientes")
+                        }
+                        // Add a single item
+                        item {
+                            PacienteItem(Paciente("Pepe", 20, "Otitis"))
+                        }
+
+                        // Add 5 items
+                        items(3) { index ->
+                            PacienteItem(Paciente("José", 56, "Vértigo"))
+                        }
+
+                        // Add another single item
+                        item {
+                            Text(text = "Last item")
+                        }
+                    }
+                    Image(
+                            painter = painterResource(id = R.drawable.pepe),
+                    contentDescription = stringResource(id = R.string.pepe_cd),
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Paciente(paciente: Paciente) {
+    Column {
+        Text(text = "Nombre: ${paciente.nombre}")
+        Text(text = "Edad: ${paciente.edad}")
+        Text(text = "Enfermedad: ${paciente.enfermedad}")
+    }
+}
+
+class Paciente(val nombre: String, val edad: Int, val enfermedad: String)
+
+@Composable
+fun PacientesList(pacientes: List<Paciente>) {
+    LazyColumn {
+        items(pacientes) { paciente ->
+            PacienteItem(paciente)
+        }
+    }
+}
+
+@Composable
+fun PacienteItem(paciente: Paciente) {
+    Column {
+        Row {
+            /*Image(
+                painter = painterResource(id = R.drawable.pepe),
+                contentDescription = stringResource(id = R.string.pepe_cd),
+            )*/
+        }
+        Row {
+            Text(paciente.nombre + ", ")
+            Text(paciente.edad.toString() + ", ")
+            Text(paciente.enfermedad)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EstadisticasOtorrinoTheme {
+        MainActivityContent()
+    }
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "EstadisticasOtorrino"
+rootProject.name = "Estadisticas Otorrino"
 include(":app")
